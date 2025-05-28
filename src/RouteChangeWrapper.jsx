c0db321e3120e98355291a3f7a1b6559cf743fc6
ACHYUTANANDA-SAHOO <achu23022002@gmail.com>
2025-05-28 23:55:03 +0530

    UI Changes

--- src/RouteChangeWrapper.jsx
+++ src/RouteChangeWrapper.jsx
@@ -7,12 +7,29 @@ function RouteChangeWrapper({ children }) {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    const path = location.pathname;
+
+    const titleMap = {
+      "/": "Home | Sort Visualizer",
+      "/bubble": "Bubble Sort | Sort Visualizer",
+      "/selection": "Selection Sort | Sort Visualizer",
+      "/insertion": "Insertion Sort | Sort Visualizer",
+      "/merge": "Merge Sort | Sort Visualizer",
+      "/quick": "Quick Sort | Sort Visualizer",
+      "/racemode": "Race Mode | Sort Visualizer",
+    };
+
+    document.title = titleMap[path] || "Sort Visualizer";
+  }, [location]);
+
+  useEffect(() => {
     setLoading(true);
+
     const timer = setTimeout(() => {
       setLoading(false);
     }, 1000);
 
-    return () => setTimeout(timer);
+    return () => clearTimeout(timer);
   }, [location]);
 
   return (
